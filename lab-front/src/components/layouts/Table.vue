13c5d66000c195cfa4b6a3e87d857796cbea7375
Nazariy <nazar260804@gmail.com>
2022-11-26 18:59:48 +0200

    added table component for entity tables

---
+++ lab-front/src/components/layouts/Table.vue
@@ -0,0 +1,23 @@
+<template>
+  <table>
+    <tr>
+      <th v-for="col in columns">
+        {{ col }}
+      </th>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Table",
+
+  props: {
+    columns: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
