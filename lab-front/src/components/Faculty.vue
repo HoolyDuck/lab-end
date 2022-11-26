c331dd1ef842ef61aa7141eda7f36318c936c908
Nazariy <nazar260804@gmail.com>
2022-11-26 19:01:38 +0200

    add faculty component

---
+++ lab-front/src/components/Faculty.vue
@@ -0,0 +1,18 @@
+<template>
+<Table :columns="columns"></Table>
+</template>
+
+<script>
+import Table from "./layouts/Table.vue";
+export default {
+  name: "Faculty",
+  components: {Table},
+  data: () => ({
+     columns: ["ID", "Name", "University"]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
