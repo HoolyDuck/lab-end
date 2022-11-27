72a4d14a62fbf141d94a904f25dca37d68789cbd
Nazariy <nazar260804@gmail.com>
2022-11-27 19:12:21 +0200

    added cross origin anotation

--- src/main/java/com/weblab/backend/controllers/FacultiesController.java
+++ src/main/java/com/weblab/backend/controllers/FacultiesController.java
@@ -4,6 +4,7 @@ package com.weblab.backend.controllers;
 import com.weblab.backend.models.Faculties;
 import com.weblab.backend.repositories.FacultiesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://127.0.0.1:5173/")
 @RequestMapping("api/faculties")
 public class FacultiesController {
 
