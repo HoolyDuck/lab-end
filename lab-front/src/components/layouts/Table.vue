26e492d091e1356ff88181f2e4a012a05398f943
Nazariy <nazar260804@gmail.com>
2022-11-27 15:47:37 +0200

    added prop to accept entity tables

--- lab-front/src/components/layouts/Table.vue
+++ lab-front/src/components/layouts/Table.vue
@@ -7,6 +7,11 @@
       <th>Edit</th>
       <th>Delete</th>
     </tr>
+    <tr v-for="row in rows">
+      <td v-for="data in row"> {{ data }}}</td>
+      <td><button>Edit</button></td>
+      <td><button>Delete</button></td>
+    </tr>
   </table>
 </template>
 
@@ -15,7 +20,11 @@ export default {
   name: "Table",
 
   props: {
-    columns: Array
+    columns: Array,
+    rows: {
+      default: [],
+      type: Array
+    }
   }
 }
 </script>
