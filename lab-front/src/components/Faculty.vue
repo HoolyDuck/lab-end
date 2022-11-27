04bf732aec31e8e0b6d45b625c9cc55932f68d58
Nazariy <nazar260804@gmail.com>
2022-11-27 19:12:01 +0200

    added method to request data from api

--- lab-front/src/components/Faculty.vue
+++ lab-front/src/components/Faculty.vue
@@ -1,15 +1,28 @@
 <template>
-<Table :columns="columns"></Table>
+  <Table :columns="columns" :rows="faculties"></Table>
 </template>
 
 <script>
 import Table from "./layouts/Table.vue";
+import FacultyService from "../services/FacultyService";
+
 export default {
   name: "Faculty",
   components: {Table},
   data: () => ({
-     columns: ["ID", "Name", "University"]
-  })
+    columns: ["ID", "Name", "University"],
+    faculties: []
+  }),
+  methods: {
+    getAllFaculties() {
+      FacultyService.getAllFaculties().then((response) => this.faculties = response.data)
+    }
+  },
+  created() {
+    this.getAllFaculties()
+  }
+
+
 }
 </script>
 
